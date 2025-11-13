9b313ccc451b55fb4dcb277961e5e3760b0d7109
deniaalzahra@gmail.com <deniaalzahra@gmail.com>
2025-11-13 15:12:06 +0700

    yes

--- script.js
+++ script.js
@@ -5,19 +5,35 @@ if (learnbtn) {
         e.preventDefault();
         const target = document.querySelector("#testimoni");
         if (target) {
-            target.scrollIntoView({behavior: "smooth"});
+            target.scrollIntoView({ behavior: "smooth" });
         }
     })
 }
 
-
 //notifikasi setelah sumbit form kontak
 const form = document.querySelector(".contact-form");
-if (form){
+if (form) {
     form.addEventListener('submit', e => {
         e.preventDefault();
         alert("pesan berasil terkirim!");
-        form.reset();   
-    
+        form.reset();
     })
 }
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll = () => {
+
+    if (document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+
+    }
+
+}
+
+scrollTopBtn.addEventListener("click", () => {
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+});
